2c2f3e3cb3f4d053f33a2f41f02d6b7fa64bd095
Christopher Moore <christopher.e.moore@gmail.com>
2022-08-17 09:26:19 +0600

    meh

--- jpeg.sh
+++ jpeg.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # don't forget to require ffi.c.stdio before ffi.jpeg
-luajit generate.lua\
-	"<jpeglib.h>" > jpeg.h
-#	-skip "<stdio.h>"\
+# for libjpeg-turbo
+# linux is using 2.0.3
+# windows is using 2.0.4 just because 2.0.3 and cmake is breaking for msvc
+luajit generate.lua "<jpeglib.h>" > jpeg.h

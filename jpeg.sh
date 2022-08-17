6da078e3c9b7ed35e945b70b2aae281886ffe3e6
thenumbernine <christopher.e.moore@gmail.com>
2022-08-17 11:46:19 +0700

    some comments

--- jpeg.sh
+++ jpeg.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 # don't forget to require ffi.c.stdio before ffi.jpeg
 # for libjpeg-turbo
-# linux is using 2.0.3
+# linux is using 2.1.2 which generates no different than 2.0.3
+#  based on apt package libturbojpeg0-dev
 # windows is using 2.0.4 just because 2.0.3 and cmake is breaking for msvc
 luajit generate.lua "<jpeglib.h>" > jpeg.h
--- tiff.sh
+++ tiff.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-# can't use tiff.h or it will get included since tiffio.h includes "tiff.h" with quotes, whose search path includes cwd .
-luajit generate.lua `pkg-config --cflags libtiff-4` "<tiffio.h>" > tiffio.h
+# apt install libtiff-dev
+luajit generate.lua `pkg-config --cflags libtiff-4` "<tiffio.h>" > tiff.h

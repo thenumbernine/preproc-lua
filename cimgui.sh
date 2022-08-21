3a5f0bce3f88d85ebd593002a64ca09d70b79203
thenumbernine <christopher.e.moore@gmail.com>
2022-08-21 11:50:47 +0700

    I guess I need this one to avoid C++ stuff in my cimgui.h header

--- cimgui.sh
+++ cimgui.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-luajit generate.lua -I../../other/cimgui  "\"cimgui.h\"" > cimgui.h
+luajit generate.lua -I../../other/cimgui -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS "\"cimgui.h\"" > cimgui.h

87599d905ce65049808517288a004efee9b4de1c
Christopher Moore <christopher.e.moore@gmail.com>
2022-12-02 18:21:21 +0700

    more

--- png.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# -skip "<limits.h>" causes "libpng requires 8-bit bytes"
-luajit generate.lua \
-	"<png.h>" > png.h
-#	-skip "<stdio.h>" \
-#	-skip "<setjmp.h>" \
-#	-skip "<time.h>" \
--- zlib.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-luajit generate.lua "<zlib.h>" > zlib.h
